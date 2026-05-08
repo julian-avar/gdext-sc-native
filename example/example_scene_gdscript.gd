2860039f8ff73f5fe7dc8dd09fb642febe48483b
Julian A Avar C <28635807+julian-avar@users.noreply.github.com>
2026-05-07 20:11:00 -0400

    .

--- example/example_scene_gdscript.gd
+++ example/example_scene_gdscript.gd
@@ -1,4 +1,19 @@
-extends CenterContainer
+
+
+
+
+
+
+class_name ExampleSceneGDScript extends CenterContainer
+var toggled := false
 
 func _ready() -> void:
 	print("Hello, GDScript!")
+	
+	var btn := get_node("Button")
+	
+	btn.connect("pressed", func():
+		toggled = !toggled
+		val tint = 0.3 if toggled else 1
+		btn.set_modulate(Color(1, tint, tint, 1))
+	)
--- example/example_scene_scala.scala
+++ example/example_scene_scala.scala
@@ -2,19 +2,19 @@ package example
 
 import gdext.core.*
 import gdext.generated.*
-import scala.scalanative.unsafe.*
 
 @gdclass
 class ExampleSceneScala extends CenterContainer:
-    private var toggled = false
+    var toggled = false
 
-    override def _ready(): Unit =
+    def _ready(): Unit =
         GdxApi.printString("Hello, Scala!")
-        val btn = findChild("Button").as(new Button(_))
-        btn.connect("pressed") { () =>
+
+        val btn = getNode("Button").as(new Button(_))
+
+        btn.connect("pressed"): () =>
             toggled = !toggled
             val tint = if toggled then 0.3f else 1f
-            btn.setModulate(Color(1f, tint, tint, 1f))
-        }
+            btn.modulate = Color(1f, tint, tint, 1f)
     end _ready
 end ExampleSceneScala
