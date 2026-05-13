d92e7081e5d0d5c7089ac518caa16f2b6e44cd33
Julian A Avar C <28635807+julian-avar@users.noreply.github.com>
2026-05-13 19:03:54 -0400

    .

--- example/rigid_body_example/player_sc.scala
+++ example/rigid_body_example/player_sc.scala
@@ -2,8 +2,8 @@ package example.rigid_body_example
 
 import gdext.core.*
 import gdext.godot.*
+import gdext.godot.Predef.*
 import gdext.generated.*
-import scala.scalanative.unsafe.*
 
 @gdclass
 class PlayerSc extends CharacterBody2D:
@@ -16,9 +16,6 @@ class PlayerSc extends CharacterBody2D:
 
     private def getInput(): Unit =
         val dir = Input.getVector("player_b_left", "player_b_right", "player_b_up", "player_b_down")
-        val vel = stackalloc[Vector2]()
-        vel.x = dir.x * speed.toFloat
-        vel.y = dir.y * speed.toFloat
-        this.velocity = vel
+        this.velocity = dir * speed.toFloat
     end getInput
 end PlayerSc
--- gdext/src/gdext/godot/Predef.scala
+++ gdext/src/gdext/godot/Predef.scala
@@ -1,3 +1,14 @@
 package gdext.godot
 
-object Predef
+import scala.scalanative.unsafe.*
+import gdext.generated.Vector2
+
+object Predef:
+    extension (v: Ptr[Vector2])
+        inline def *(scalar: Float): Ptr[Vector2] =
+            val result = stackalloc[Vector2]()
+            result.x = v.x * scalar
+            result.y = v.y * scalar
+            result
+    end extension
+end Predef
